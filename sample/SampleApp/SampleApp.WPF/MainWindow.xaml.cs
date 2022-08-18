588e33515ebe1368073e8a44931aa4f3ef897967
Tim Miller <innerlogic4321@gmail.com>
2022-08-18 11:29:05 -0400

    Add WPF sample

---
+++ sample/SampleApp/SampleApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Drastic.UI;
+using Drastic.UI.Platform.WPF;
+
+namespace SampleApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UI.Init();
+            LoadApplication(new SampleApp.App());
+        }
+    }
+}
